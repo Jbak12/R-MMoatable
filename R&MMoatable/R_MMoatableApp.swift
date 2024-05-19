8e5544373cb8bab9cbb2fe827ed27b54149bd268
Jakub Bakalarz <kubokik5@gmail.com>
2024-05-19 23:36:22 +0200

    Initial Commit

---
+++ R&MMoatable/R_MMoatableApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_MMoatableApp.swift
+//  R&MMoatable
+//
+//  Created by Jakub Bakalarz on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct R_MMoatableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
